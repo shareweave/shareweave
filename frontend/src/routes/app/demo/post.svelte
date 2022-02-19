f5b0f9d741e2263e52cd73217319db5903e074b8
Ian <code3@vivaldi.net>
2022-02-18 19:31:56 -0500

    fix: global vars

--- frontend/src/routes/app/demo/post.svelte
+++ frontend/src/routes/app/demo/post.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Shareweave from 'shareweave';
 	const posts = new Shareweave('shareweave-demo-blog').posts;
-	globalThis.posts = posts;
+	// globalThis.posts = posts;
 </script>
 
 <h1>Post Demo</h1>
