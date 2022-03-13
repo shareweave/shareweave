760bbf9d5b423e9e45cbfa8a6c1c4975c0b3ce7d
Ian <code3@vivaldi.net>
2022-03-13 14:49:44 -0400

    older fetchIndex

--- shareweave/src/utils/gunIndex.ts
+++ shareweave/src/utils/gunIndex.ts
@@ -1,27 +1,34 @@
-import { IGunChainReference } from "gun/types/chain"
+import type { IGun, IGunChain } from 'gun'
 import { gun, keys, SEA, index } from "../gun"
 import { subscribe } from "../store"
 import getCurrentDateString from "./date"
 
-let dataset: string
-subscribe(a => dataset = a.dataset || '')
-export async function index(chainReference: IGunChainReference, indexes: keys[]) {
-    // @ts-expect-error incorrect gun types
-    const data = gun.user()["_"]['soul']
-    const hash = await SEA.work(data, null, undefined, { name: "SHA-256" })
+let appName: string
+subscribe(a => appName = a.appName || '')
+export async function index(chainReference: any, indexes: keys[]) {
+    const data = chainReference._.soul || chainReference.load()._.link
+    console.log(chainReference)
+    const hash = await SEA.work(data, null, null, { name: "SHA-256" })
     if (!hash) throw new Error('hashing failed')
     indexes.forEach(index => {
-        gun.get(dataset).get(index).get(getCurrentDateString()).get('#').get(hash).put(data)
+        console.log('indexing', index)
+        gun.get(appName).get(index).get(getCurrentDateString()).get('#').get(hash).put(data, ack => {
+            // @ts-expect-error incorrect gun types
+            if (ack.err) throw new Error(index + ack.err)
+            console.log(index, hash, data, JSON.stringify(hash), gun.get(appName).get(index).get(getCurrentDateString()).get('#'), 'done')
+        })
     })
 }
 
 function fetchIndexFromGun(index: keys) {
     return new Promise<index>((resolve, reject) => {
-        gun.get(dataset).get(index).load!(data => resolve(data))
+        gun.get(appName).get(index).load(() => {
+            gun.get(appName).get(index).load(data => resolve(data))
+        })
     })
 }
 
-export async function fetchIndex(index: keys) {
+export async function fetchIndex(index: keys): Promise<string[]> {
     const data = await fetchIndexFromGun(index)
     await new Promise<void>(resolve => setInterval(() => {
         if (data) resolve()
@@ -29,26 +36,34 @@ export async function fetchIndex(index: keys) {
     //await (await fetchIndexFromGun(index))
 
 
-    await console.log(await fetchIndexFromGun(index))
     await console.log('fetchindex', data)
-    let result: string[] = []
-    const items = await Object.values(data)
-    await console.log('items', items)
-    for (let [index, item] of items.entries()) {
-        for (let piece of Object.values(item)) {
-            if (!(typeof piece === 'string')) {
-                for (let item of Object.entries(piece)) {
-                    await result.push(item[1])
+    /* let result: string[] = []
+     const items = await Object.values(data)
+     await console.log('items', items) */
+
+    // we have an object with times as keys, but we don't need the times rn, just get values
+    Object.values(data)
+        // now we have an array of objects with the '#' property containing one or more hash/data pairs
+        .map(item => Object.values(item)[0])
+        // now we have an array with objects containing one or more hash/value pairs, we'll turn it into an array with arrays of values
+        .map(item => Object.values(item))
+        // now flatten it into an array values
+        .reduce((previousResult, item) => {
+            console.log(item)
+            item.forEach(item => previousResult.push(item))
+        }, [])
+
+    /*    for (let [index, item] of items.entries()) {
+            console.log('list', index, item)
+            for (let piece of Object.values(item)) {
+                console.log(piece, item, index, 'piece')
+                if (!(typeof piece === 'string')) {
+                    for (let item of Object.entries(piece)) {
+                        await result.push(item[1])
+                    }
                 }
             }
         }
-    }
-    console.log('result', result)
-    return result
-    /*   const result: string[] = []
-       gun.get(dataset).get(index).map().get('#').map().once(data => {
-           console.log('data t')
-           if (!(data == undefined)) result.push((data as unknown) as string)
-       }, { wait: 10000 })
-       return result */
+        console.log('result', result)
+        return result*/
 }
